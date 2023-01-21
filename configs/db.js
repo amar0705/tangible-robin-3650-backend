03676948e3f08b16b2b405a866ecf807d4e5fab7
AMAR <amar@AMARs-MacBook-Pro.local>
2023-01-21 17:33:25 +0530

    tangible-robin-3650-backend

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connection = mongoose.connect("mongodb+srv://amarjeet:amarjeet@cluster0.b4efj1j.mongodb.net/betterbuys?retryWrites=true&w=majority")
+let mongoURL =
+  "mongodb+srv://amarjeet:amarjeet@cluster0.b4efj1j.mongodb.net/betterbuys?retryWrites=true&w=majority";
+const connection = mongoose.connect(mongoURL);
 
-module.exports = {connection}
+module.exports = { connection };
